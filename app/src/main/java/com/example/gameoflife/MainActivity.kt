f642c3d6b7cfd7d2fe63d3ed177ff4441e7ff8b3
Ivan <ivan98angelov@gmail.com>
2021-03-12 17:05:00 +0200

    Code moved to fragments, and navigation graph added

--- app/src/main/java/com/example/gameoflife/MainActivity.kt
+++ app/src/main/java/com/example/gameoflife/MainActivity.kt
@@ -1,50 +1,5 @@
 package com.example.gameoflife
 
-import android.annotation.SuppressLint
-import android.os.Bundle
-import android.os.Handler
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("ResourceAsColor")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        lateinit var timer: Timer
-        val cellMap: LinearLayout = findViewById(R.id.pixel_grid)
-        val clearButton: Button = findViewById(R.id.clear_button)
-        val playButton: Button = findViewById(R.id.play_button)
-        val randomButton: Button = findViewById(R.id.random_button)
-        val stopButton: Button = findViewById(R.id.stop_button)
-        val pixelGrid = PixelGrid(this)
-
-        cellMap.addView(pixelGrid)
-        clearButton.setOnClickListener { pixelGrid.clearGrid() }
-        randomButton.setOnClickListener { pixelGrid.generateRandom() }
-        playButton.setOnClickListener {
-            playButton.isEnabled = false
-            stopButton.isEnabled = true
-            val handler = Handler()
-            timer = Timer()
-            val timerTask = object : TimerTask() {
-                override fun run() {
-                    handler.post {
-                        pixelGrid.gameOfLifeRules()
-                    }
-                }
-            }
-            timer.schedule(timerTask, 0, 100)
-        }
-
-        stopButton.setOnClickListener {
-            stopButton.isEnabled = false
-            playButton.isEnabled = true
-            timer.cancel()
-        }
-    }
-
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
