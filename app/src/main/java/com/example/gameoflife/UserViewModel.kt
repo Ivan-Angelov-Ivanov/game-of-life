13ce276e1f4c90f1a0ae20b92fc7cf175672acd9
Ivan <ivan98angelov@gmail.com>
2021-03-12 17:11:47 +0200

    User Repository and ViewModel added

---
+++ app/src/main/java/com/example/gameoflife/UserViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.gameoflife
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class UserViewModel(private val repository: UserRepository): ViewModel() {
+    val allUsers: LiveData<List<User>> = repository.allUsers
+
+    fun insert(user: User) = viewModelScope.launch {
+        repository.insert(user)
+    }
+}
+
+class UserViewModelFactory(private val repository: UserRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
