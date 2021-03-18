f422df027396b4788b30556a3235fcec7c9a8fca
Ivan <ivan98angelov@gmail.com>
2021-03-18 15:53:38 +0200

    Declared post requset function

--- app/src/main/java/com/example/gameoflife/UserRepository.kt
+++ app/src/main/java/com/example/gameoflife/UserRepository.kt
@@ -2,6 +2,9 @@ package com.example.gameoflife
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import com.example.gameoflife.api.RetrofitInstance
+import com.example.gameoflife.roomdb.User
+import com.example.gameoflife.roomdb.UserDao
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
@@ -14,5 +17,9 @@ class UserRepository(private val userDao: UserDao) {
     @WorkerThread
     suspend fun insert(user: User) {
         userDao.insertAll(user)
+    }
+
+    suspend fun postUser(user: User) {
+        RetrofitInstance.api.addUser(user)
     }
 }
